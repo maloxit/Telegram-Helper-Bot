ef4b31f04618bc9c01c60847c2aea6ef042cf6f7
maloxit <61049974+maloxit@users.noreply.github.com>
2021-04-24 00:29:01 +0300

    Base project

---
+++ TelegramHelperBot/TelegramHelperBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramHelperBot
+{
+    class Program
+    {
+        static DataBaseManager dbManager;
+        static UserSessionsManager usManager;
+        static HelperBot hBot;
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
