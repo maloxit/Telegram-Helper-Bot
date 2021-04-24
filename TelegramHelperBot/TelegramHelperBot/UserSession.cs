c8b710ad9ea971d896f6308ecf203285467acbee
maloxit <61049974+maloxit@users.noreply.github.com>
2021-04-25 00:29:11 +0300

    Added UserSessionsManager class code and UserSession class testing code + fixes

--- TelegramHelperBot/TelegramHelperBot/UserSession.cs
+++ TelegramHelperBot/TelegramHelperBot/UserSession.cs
@@ -9,6 +9,54 @@ namespace TelegramHelperBot
 {
     class UserSession
     {
-        public Task<Message> lastSendMessageTask { get; internal set; }
+        public long chatId;
+        public int lastUpdateOffset;
+        public UserSession(long chatId)
+        {
+            this.chatId = chatId;
+            lastUpdateOffset = -1;
+        }
+
+
+        public void ProcessUpdate(DataBaseManager dbManager, long chatId, Update upd, int offset, out ReplytRequest replytRequest, out MarkupEditRequest editRequest)
+        {
+            lastUpdateOffset = offset;
+            editRequest = null;
+            switch (upd.Type)
+            {
+                case Telegram.Bot.Types.Enums.UpdateType.CallbackQuery:
+                    editRequest = new MarkupEditRequest(chatId, upd.CallbackQuery.Message.MessageId);
+                    if (upd.CallbackQuery.Data == "opt1")
+                    {
+                        replytRequest = new ReplytRequest(chatId, "Вы кликнули тут", ("opt1", "1 Кликни тут"), ("opt2", "2 Или вот тут"), ("opt3", "3 Или даже здесь"));
+                    }
+                    else if (upd.CallbackQuery.Data == "opt2")
+                    {
+                        replytRequest = new ReplytRequest(chatId, "Вы кликнули вот тут", ("opt2", "2 Или вот тут"), ("opt3", "3 Или даже здесь"));
+                    }
+                    else if (upd.CallbackQuery.Data == "opt3")
+                    {
+                        replytRequest = new ReplytRequest(chatId, "Вы кликнули даже здесь", ("opt1", "1 Кликни тут"), ("opt2", "2 Или вот тут"));
+                    }
+                    else
+                    {
+                        replytRequest = new ReplytRequest(chatId, "Вы кликнули непойми где", ("opt1", "1 Кликни тут"));
+                    }
+                    break;
+                case Telegram.Bot.Types.Enums.UpdateType.Message:
+                    if (upd.Message.Text == "/start")
+                    {
+                        replytRequest = new ReplytRequest(chatId, "Для начала", ("opt1", "1 Кликни тут"));
+                    }
+                    else
+                    {
+                        replytRequest = new ReplytRequest(chatId, "Зачем вы написали" + upd.Message.Text + "?");
+                    }
+                    break;
+                default:
+                    replytRequest = new ReplytRequest(chatId, "Зачем вы это сделали?");
+                    break;
+            }
+        }
     }
 }
--- TelegramHelperBot/TelegramHelperBot/UserSessionsManager.cs
+++ TelegramHelperBot/TelegramHelperBot/UserSessionsManager.cs
@@ -12,57 +12,132 @@ namespace TelegramHelperBot
     class UserSessionsManager
     {
         DataBaseManager dbManager;
-        Dictionary<ChatId, UserSession> activeSessions;
+        Dictionary<long, UserSession> activeSessions;
 
         public UserSessionsManager(DataBaseManager dbManager)
         {
             this.dbManager = dbManager;
-            activeSessions = new Dictionary<ChatId, UserSession>(50);
+            activeSessions = new Dictionary<long, UserSession>(50);
+            nextMarkupEditRequests = new List<MarkupEditRequest>();
+            nextReplytRequests = new List<ReplytRequest>();
         }
 
-        //Доступ к конкретным сессиям по id чата из вне
-        public UserSession this[ChatId chatId]
+        //Массив запросов на редактирование предыдущих сообщений (в часности отключение inline кнопок)
+        public List<MarkupEditRequest> nextMarkupEditRequests;
+
+        //Массив запросов на отправку сообщений
+        public List<ReplytRequest> nextReplytRequests;
+
+        static long GetChatIdOfUpdate(Update update)
         {
-            get
+            switch (update.Type)
             {
-                return activeSessions[chatId];
+                case UpdateType.Message:
+                    return update.Message.Chat.Id;
+                case UpdateType.CallbackQuery:
+                    return update.CallbackQuery.Message.Chat.Id;
+                default:
+                    throw new Exception("Not supported update type.");
             }
         }
 
-        //Возвращает массив запросов на редактирование предыдущих сообщений (в часности отключение inline кнопок), просматривая activeSessions
-        public MarkupEditRequest[] nextMarkupEditRequests { get; }
-
-        //Возвращает массив запросов на отправку сообщений, просматривая activeSessions
-        public ReplytRequest[] nextReplytRequests { get; }
-
         //Обрабатывает ответы в чатах из activeSessions, если первое сообщение, то создает новую сессию
         //Если от одного пользователя было получено более одного сообщения, то обрабатывается только первое
-        public void ProcessNextUpdates(Update[] nextUpdates)
+        //Для этого в каждой сессии хранится оффсет - id первого апдейта последней обработанной им группы
+        public void ProcessNextUpdates(Update[] nextUpdates, int offset)
         {
-            
-            throw new NotImplementedException();
+            nextReplytRequests.Clear();
+            nextMarkupEditRequests.Clear();
+            foreach (var upd in nextUpdates)
+            {
+                long chatId;
+                try
+                {
+                    chatId = GetChatIdOfUpdate(upd);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                UserSession session;
+                if (activeSessions.ContainsKey(chatId))
+                {
+                    session = activeSessions[chatId];
+                }
+                else
+                {
+                    session = new UserSession(chatId);
+                    activeSessions.Add(chatId, session);
+                }
+
+                if (session.lastUpdateOffset != offset)
+                {
+                    session.ProcessUpdate(dbManager, chatId, upd, offset, out ReplytRequest replytRequest, out MarkupEditRequest editRequest);
+                    if (replytRequest != null) nextReplytRequests.Add(replytRequest);
+                    if (editRequest != null) nextMarkupEditRequests.Add(editRequest);
+                }
+                else
+                {
+                    continue;
+                }
+            }
         }
 
         //Удаляет из activeSessions сессии, которые не были активны долгое время
-        internal void RemoveOld()
+        public void RemoveOld()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 
     //Хранит данные, которые нужно передать боту для отправки ответа
     public class ReplytRequest
     {
-        public readonly ChatId chatId;
+        public readonly long chatId;
         public readonly string text;
-        public readonly ParseMode replyMarkup;
+        public readonly InlineKeyboardMarkup replyMarkup;
+
+        public ReplytRequest(long chatId, string text, params (string, string) [] options)
+        {
+            this.chatId = chatId;
+            this.text = text;
+            List<InlineKeyboardButton> keyboard = new List<InlineKeyboardButton>(options.Length);
+            foreach (var opt in options)
+            {
+                var button = new InlineKeyboardButton
+                {
+                    CallbackData = opt.Item1,
+                    Text = opt.Item2
+                };
+                keyboard.Add(button);
+            }
+            replyMarkup = new InlineKeyboardMarkup(keyboard);
+        }
     }
 
     //Хранит данные, которые нужно передать боту для изменения (отключения) inline кнопок в отправленном ранее сообщении
     public class MarkupEditRequest
     {
-        public readonly ChatId chatId;
+        public readonly long chatId;
         public readonly int inlineMessageId;
         public readonly InlineKeyboardMarkup replyMarkup;
+
+        public MarkupEditRequest(long chatId, int inlineMessageId, params (string, string)[] options)
+        {
+            this.chatId = chatId;
+            this.inlineMessageId = inlineMessageId;
+            List<InlineKeyboardButton> keyboard = new List<InlineKeyboardButton>(options.Length);
+            foreach (var opt in options)
+            {
+                var button = new InlineKeyboardButton
+                {
+                    CallbackData = opt.Item1,
+                    Text = opt.Item2
+                };
+                keyboard.Add(button);
+            }
+            replyMarkup = new InlineKeyboardMarkup(keyboard);
+        }
     }
 }
